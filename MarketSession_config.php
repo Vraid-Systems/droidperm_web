91056eaaa92ab2c1d8d0a7f96241040543314aef
Jason Zerbe <jzerbe@vraidsys.com>
2011-11-19 22:22:17 -0600

    move to much more simple system, as hosting environment does not want to play nice with CodeIgniter MVC framework

---
+++ MarketSession_config.php
@@ -0,0 +1,7 @@
+<?php
+
+//configure Google Account info and Android device - working throw-away values
+define('GOOGLE_EMAIL', 'xdbgqfucrdwsqu@mailinator.com');
+define('GOOGLE_PASSWD', 'xdbgqfucrdwsqu@xdbgqfucrdwsqu');
+define('ANDROID_DEVICEID', '0123456789123456');
+?>
--- index.php
+++ index.php
@@ -1,205 +1,96 @@
 <?php
 
-/*
- *---------------------------------------------------------------
- * APPLICATION ENVIRONMENT
- *---------------------------------------------------------------
- *
- * You can load different configurations depending on your
- * current environment. Setting the environment also influences
- * things like logging and error reporting.
- *
- * This can be set to anything, but default usage is:
- *
- *     development
- *     testing
- *     production
- *
- * NOTE: If you change these, also change the error_reporting() code below
- *
- */
-	define('ENVIRONMENT', 'development');
-/*
- *---------------------------------------------------------------
- * ERROR REPORTING
- *---------------------------------------------------------------
- *
- * Different environments will require different levels of error reporting.
- * By default development will show errors but testing and live will hide them.
- */
-
-if (defined('ENVIRONMENT'))
-{
-	switch (ENVIRONMENT)
-	{
-		case 'development':
-			error_reporting(E_ALL);
-		break;
-	
-		case 'testing':
-		case 'production':
-			error_reporting(0);
-		break;
-
-		default:
-			exit('The application environment is not set correctly.');
-	}
+# get the package we are going to be searching for
+$myPackageNameSearch = '';
+if (isset($_GET['package']) && ($_GET['package'] != '')) {
+    $myPackageNameSearch = $_GET['package'];
+}
+if ($myPackageNameSearch == '') {
+    die('package arg is empty');
 }
 
-/*
- *---------------------------------------------------------------
- * SYSTEM FOLDER NAME
- *---------------------------------------------------------------
- *
- * This variable must contain the name of your "system" folder.
- * Include the path if the folder is not in the same  directory
- * as this file.
- *
- */
-	$system_path = 'system';
+# exact string matching or match all strings that have package substring
+$myExactMatchBoolean = false;
+if (isset($_GET['exact']) && ($_GET['exact'] != '')) {
+    $myExactMatchBoolean = $_GET['exact'];
+}
+if (!empty($myExactMatchBoolean)) {
+    $myExactMatchBoolean = true;
+}
 
-/*
- *---------------------------------------------------------------
- * APPLICATION FOLDER NAME
- *---------------------------------------------------------------
- *
- * If you want this front controller to use a different "application"
- * folder then the default one you can set its name here. The folder
- * can also be renamed or relocated anywhere on your server.  If
- * you do, use a full server path. For more info please see the user guide:
- * http://codeigniter.com/user_guide/general/managing_apps.html
- *
- * NO TRAILING SLASH!
- *
- */
-	$application_folder = 'application';
+# includes must be in this order, especially middle two
+include("MarketSession_config.php");
+include("proto/protocolbuffers.inc.php");
+include("proto/market.proto.php");
+include("MarketSession.php");
 
-/*
- * --------------------------------------------------------------------
- * DEFAULT CONTROLLER
- * --------------------------------------------------------------------
- *
- * Normally you will set your default controller in the routes.php file.
- * You can, however, force a custom routing by hard-coding a
- * specific controller class/function here.  For most applications, you
- * WILL NOT set your routing here, but it's an option for those
- * special instances where you might want to override the standard
- * routing in a specific front controller that shares a common CI installation.
- *
- * IMPORTANT:  If you set the routing here, NO OTHER controller will be
- * callable. In essence, this preference limits your application to ONE
- * specific controller.  Leave the function name blank if you need
- * to call functions dynamically via the URI.
- *
- * Un-comment the $routing array below to use this feature
- *
- */
-	// The directory name, relative to the "controllers" folder.  Leave blank
-	// if your controller is not in a sub-folder within the "controllers" folder
-	// $routing['directory'] = '';
+# create session, and auth with google servers --> auth token
+$session = new MarketSession();
+$session->login(GOOGLE_EMAIL, GOOGLE_PASSWD);
+$session->setAndroidId(ANDROID_DEVICEID);
 
-	// The controller class file name.  Example:  Mycontroller
-	// $routing['controller'] = '';
+# create market search request
+$ar = new AppsRequest();
+$ar->setQuery($thePackageName);
+$ar->setOrderType(AppsRequest_OrderType::NONE);
+$ar->setStartIndex(0);
+$ar->setEntriesCount(5);
+$ar->setWithExtendedInfo(true);
 
-	// The controller function you wish to be called.
-	// $routing['function']	= '';
+$reqGroup = new Request_RequestGroup();
+$reqGroup->setAppsRequest($ar);
 
+# query the Android market servers using request and auth token
+$response = $session->execute($reqGroup);
 
-/*
- * -------------------------------------------------------------------
- *  CUSTOM CONFIG VALUES
- * -------------------------------------------------------------------
- *
- * The $assign_to_config array below will be passed dynamically to the
- * config class when initialized. This allows you to set custom config
- * items or override any default config values found in the config.php file.
- * This can be handy as it permits you to share one application between
- * multiple front controller files, with each file containing different
- * config values.
- *
- * Un-comment the $assign_to_config array below to use this feature
- *
- */
-	// $assign_to_config['name_of_config_item'] = 'value of config item';
+# get the response
+$groups = $response->getResponsegroupArray();
 
+#parse the response into arrays
+$i = 0;
+foreach ($groups as $rg) {
+    $appsResponse = $rg->getAppsResponse();
+    $apps = $appsResponse->getAppArray();
+    foreach ($apps as $app) {
+        $aPackageName = $app->getPackageName();
+        if ($theExactMatchBoolean) {
+            //check if this packagename is substring of search package name
+            $pos = strpos($thePackageName, $aPackageName);
+        } else {
+            //check if search package name is substring of this package name
+            $pos = strpos($aPackageName, $thePackageName);
+        }
+        if ($pos !== false) { //if there is a match ...
+            $packages[$i]['package_name'] = $aPackageName;
+            $packages[$i]['package_id'] = $app->getId();
+            $packages[$i]['package_category'] = $app->getExtendedInfo()->getCategory();
+            $packages[$i]['package_perm_array'] = $app->getExtendedInfo()->getPermissionIdArray();
+            $i++;
+        }
+    }
+}
 
-
-// --------------------------------------------------------------------
-// END OF USER CONFIGURABLE SETTINGS.  DO NOT EDIT BELOW THIS LINE
-// --------------------------------------------------------------------
-
-/*
- * ---------------------------------------------------------------
- *  Resolve the system path for increased reliability
- * ---------------------------------------------------------------
- */
-
-	// Set the current directory correctly for CLI requests
-	if (defined('STDIN'))
-	{
-		chdir(dirname(__FILE__));
-	}
-
-	if (realpath($system_path) !== FALSE)
-	{
-		$system_path = realpath($system_path).'/';
-	}
-
-	// ensure there's a trailing slash
-	$system_path = rtrim($system_path, '/').'/';
-
-	// Is the system path correct?
-	if ( ! is_dir($system_path))
-	{
-		exit("Your system folder path does not appear to be set correctly. Please open the following file and correct this: ".pathinfo(__FILE__, PATHINFO_BASENAME));
-	}
-
-/*
- * -------------------------------------------------------------------
- *  Now that we know the path, set the main path constants
- * -------------------------------------------------------------------
- */
-	// The name of THIS file
-	define('SELF', pathinfo(__FILE__, PATHINFO_BASENAME));
-
-	// The PHP file extension
-	// this global constant is deprecated.
-	define('EXT', '.php');
-
-	// Path to the system folder
-	define('BASEPATH', str_replace("\\", "/", $system_path));
-
-	// Path to the front controller (this file)
-	define('FCPATH', str_replace(SELF, '', __FILE__));
-
-	// Name of the "system folder"
-	define('SYSDIR', trim(strrchr(trim(BASEPATH, '/'), '/'), '/'));
-
-
-	// The path to the "application" folder
-	if (is_dir($application_folder))
-	{
-		define('APPPATH', $application_folder.'/');
-	}
-	else
-	{
-		if ( ! is_dir(BASEPATH.$application_folder.'/'))
-		{
-			exit("Your application folder path does not appear to be set correctly. Please open the following file and correct this: ".SELF);
-		}
-
-		define('APPPATH', BASEPATH.$application_folder.'/');
-	}
-
-/*
- * --------------------------------------------------------------------
- * LOAD THE BOOTSTRAP FILE
- * --------------------------------------------------------------------
- *
- * And away we go...
- *
- */
-require_once BASEPATH.'core/CodeIgniter.php';
-
-/* End of file index.php */
-/* Location: ./index.php */
+# output the response - parse and output kept seperate for easy move to MVC later
+$packages_count = sizeof($packages);
+echo 'package_count=' . $packages_count . "\n";
+for ($i = 0; $i < $packages_count; $i++) {
+    echo 'name=' . $packages[$i]['package_name'] . "\n";
+    echo 'id=' . $packages[$i]['package_id'] . "\n";
+    echo 'category=' . $packages[$i]['package_category'] . "\n";
+    $package_perm_array = $packages[$i]['package_perm_array'];
+    $ppa_size = sizeof($package_perm_array);
+    echo 'perms=';
+    for ($i2 = 0; $i2 < $ppa_size; $i2++) {
+        $perm = $package_perm_array[$i2];
+        if ($perm != '') {
+            if (($i2 + 1) == $ppa_size) {
+                echo $perm;
+            } else {
+                echo $perm . ',';
+            }
+        }
+    }
+    echo "\n";
+}
+?>
+  
